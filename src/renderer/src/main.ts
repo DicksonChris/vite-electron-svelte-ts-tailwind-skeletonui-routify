23d581decc2e5c463470b8760ba05429ed1d8adf
DicksonChris <70047029+DicksonChris@users.noreply.github.com>
2023-05-09 22:44:58 -0500

    Initial commit adds skeleton ui

---
+++ src/renderer/src/main.ts
@@ -0,0 +1,14 @@
+// The ordering of these imports is critical to your app working properly
+import '@skeletonlabs/skeleton/themes/theme-skeleton.css'
+// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+import '@skeletonlabs/skeleton/themes/theme-skeleton.css'
+// Most of your app wide CSS should be put in this file
+import '@skeletonlabs/skeleton/styles/skeleton.css';
+import './app.postcss'
+import App from './App.svelte'
+
+const app = new App({
+  target: document.getElementById('app')
+})
+
+export default app
